7660bfe8b298ea436feba26b2c183a4b23ae0716
Soumaditya Pal <dev.soumadityapal@outlook.com>
2025-06-12 18:54:52 +0530

    feat(footer): add footer component with social links and navigation
    - Add react-icons dependency for footer social icons
    - Create FooterSecond component with brand details, social links, and navigation sections
    - Integrate footer into layout component

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavbarDemo } from "@/app/components/ui/Navbar/Page";
+import FooterSecond from "@/app/components/ui/Footer/Page";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavbarDemo/>
+        
         {children}
+        <FooterSecond/>
       </body>
     </html>
   );
--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroWithCarousel from "@/app/components/ui/HeroWithCarousel";
 import Feature from "@/app/components/ui/Feature/Page";
-
 export default function Home() {
   return (  
     <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-8 pt-32 sm:pt-40 md:pt-48 pb-8">
